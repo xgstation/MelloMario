7487c366d057b0b0078e3c7df19756fa6008d2c6
Sergey Maltsev <maltsev.1@osu.edu>
2017-09-24 14:25:40 -0400

    Combining multiple sprite factories into one.

--- MelloMario/MelloMario/Factory/SpriteFactory.cs
+++ MelloMario/MelloMario/Factory/SpriteFactory.cs
@@ -26,6 +26,15 @@ namespace MelloMario
         //standard
         private Texture2D StandardCrouchingLeft, StandardFallingLeft, StandardIdleLeft, StandardJumpingLeft, StandardWalkingLeft;
         private Texture2D StandardCrouchingRight, StandardFallingRight, StandardIdleRight, StandardJumpingRight, StandardWalkingRight;
+
+        private Texture2D goombaSpritesheet;
+        private Texture2D greenKoopaSpritesheet;
+        private Texture2D redKoopaSpritesheet;
+        private Texture2D superMushroomSpritesheet;
+        private Texture2D oneUpMushroomSpritesheet;
+        private Texture2D coinSpritesheet;
+        private Texture2D fireFlowerSpritesheet;
+        private Texture2D starSpritesheet;
         private static SpriteFactory instance = new SpriteFactory();
         public SpriteFactory()
         {
@@ -39,6 +48,14 @@ namespace MelloMario
         }
         public void LoadAllTextures(ContentManager content)
         {
+            goombaSpritesheet = content.Load<Texture2D>("goomba");
+            greenKoopaSpritesheet = content.Load<Texture2D>("greenKoopa");
+            redKoopaSpritesheet = content.Load<Texture2D>("redKoopa");
+            superMushroomSpritesheet = content.Load<Texture2D>("superMushroom");
+            oneUpMushroomSpritesheet = content.Load<Texture2D>("1-UpMushroom");
+            coinSpritesheet = content.Load<Texture2D>("coin");
+            fireFlowerSpritesheet = content.Load<Texture2D>("fireFlower");
+            starSpritesheet = content.Load<Texture2D>("star");
             //dead
             Dead = content.Load<Texture2D>("Dead");
             //fire
@@ -110,6 +127,21 @@ namespace MelloMario
                 sprite = new AnimatedSprite(stringToTexture[textureName]);
             }
             return sprite;
+        }
+
+        public ISprite CreateGoombaSprite()
+        {
+            return new GoombaSprite(goombaSpritesheet, 1, 2);
+        }
+
+        public ISprite CreateKoopaSprite()
+        {
+            return new KoopaSprite(greenKoopaSpritesheet, redKoopaSpritesheet, 1, 6, 3, 4);
+        }
+
+        public ISprite CreateItemSprite()
+        {
+            return new ItemSprite(superMushroomSpritesheet, oneUpMushroomSpritesheet, starSpritesheet, fireFlowerSpritesheet, coinSpritesheet, 1, 4);
         }
     }
 }
