eac1158a6e2118c4bc472e900d716dfc47ceb74d
hcz <h.cz@qq.com>
2017-09-24 15:07:42 -0400

    Add CommandFactory

---
+++ MelloMario/MelloMario/Factory/CommandFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MelloMario
+{
+    class CommandFactory : ICommandFactory
+    {
+        ICommand createGameModelCommand(string actionName, GameModel model)
+        {
+            // TODO: implement it
+        }
+        ICommand createMarioCommand(string actionName, Mario mario)
+        {
+            // TODO: implement it
+        }
+    }
+}
