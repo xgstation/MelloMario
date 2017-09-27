8d1f779abd69bdff820c46e912a992866f532d8b
Yegie <yegres.vestlam@gmail.com>
2017-09-26 22:26:32 -0400

    changed update to regular for loop

--- MelloMario/MelloMario/Theming/GameModel.cs
+++ MelloMario/MelloMario/Theming/GameModel.cs
@@ -52,9 +52,9 @@ namespace MelloMario
             {
                 controller.Update();
             }
-            foreach (IGameObject gameObject in objects)
+            for(int i = 0; i < objects.Count; ++i)
             {
-                gameObject.Update(gameTime);
+                objects[i].Update(gameTime);
             }
 
         }
