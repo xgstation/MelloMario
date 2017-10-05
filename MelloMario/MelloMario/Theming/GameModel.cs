2857cca639bb593649441c95af634578019ab9f8
Sergey Maltsev <maltsev.1@osu.edu>
2017-10-04 23:09:47 -0400

    Changed model to seperate blocks from objects that can move.

--- MelloMario/MelloMario/Theming/GameModel.cs
+++ MelloMario/MelloMario/Theming/GameModel.cs
@@ -11,12 +11,14 @@ namespace MelloMario
     class GameModel
     {
         private List<IController> controllers;
-        private IGameObject[,] objects;
+        private IGameObject[,] stationaryObjects;
+        private List<IGameObject> dynamicObjects;
         // TODO: Do we need another abstraction layer for mario's actions?
         private Mario mario;
 
         public GameModel()
         {
+            dynamicObjects = new List<IGameObject>();
         }
 
         public void LoadControllers(List<IController> controllers)
@@ -26,33 +28,33 @@ namespace MelloMario
 
         public void Bind(GameScript script)
         {
-            script.Bind(controllers, mario, objects);
+            script.Bind(controllers, mario, stationaryObjects);
         }
 
         public void LoadEntities(int w, int h)
         {
-            objects = new IGameObject[w, h];
+            stationaryObjects = new IGameObject[w, h];
             mario = new Mario(new Point(100, 100));
 
             //all of these array cords are randomly picked ignore them
-            objects[0, 0] = mario;
+            stationaryObjects[0, 0] = mario;
             //temporary hard coded blocks
             Brick blockHidden = new Brick(new Point(300, 200));
             blockHidden.State.Hide();
-            objects[1, 0] = (blockHidden);
-            objects[2, 0] = (new Brick(new Point(100, 200)));
-            objects[3, 0] = (new Question(new Point(150, 200)));
-            objects[4, 0] = (new Stair(new Point(200, 200)));
-            objects[0, 1] = (new Floor(new Point(250, 200)));
-            objects[0, 2] = (new Goomba(new Point(100, 300)));
-            objects[0, 3] = (new GreenKoopa(new Point(100, 400)));
-            objects[0, 4] = (new RedKoopa(new Point(200, 400)));
-            objects[1, 1] = (new Coin(new Point(50, 50)));
-            objects[1, 2] = (new FireFlower(new Point(100, 50)));
-            objects[1, 3] = (new OneUpMushroom(new Point(200, 50)));
-            objects[1, 4] = (new SuperMushroom(new Point(250, 50)));
-            objects[2, 1] = (new Star(new Point(300, 50)));
-            objects[2, 2] = (new Pipeline(new Point(350, 168)));
+            stationaryObjects[1, 0] = (blockHidden);
+            stationaryObjects[2, 0] = (new Brick(new Point(100, 200)));
+            stationaryObjects[3, 0] = (new Question(new Point(150, 200)));
+            stationaryObjects[4, 0] = (new Stair(new Point(200, 200)));
+            stationaryObjects[0, 1] = (new Floor(new Point(250, 200)));
+            stationaryObjects[2, 2] = (new Pipeline(new Point(350, 168)));
+            stationaryObjects[1, 1] = (new Coin(new Point(50, 50)));
+            dynamicObjects.Add(new Goomba(new Point(100, 300)));
+            dynamicObjects.Add(new GreenKoopa(new Point(100, 400)));
+            dynamicObjects.Add(new RedKoopa(new Point(200, 400)));
+            dynamicObjects.Add(new FireFlower(new Point(100, 50)));
+            dynamicObjects.Add(new OneUpMushroom(new Point(200, 50)));
+            dynamicObjects.Add(new SuperMushroom(new Point(250, 50)));
+            dynamicObjects.Add(new Star(new Point(300, 50)));
 
         }
 
@@ -63,15 +65,20 @@ namespace MelloMario
                 controller.Update();
             }
 
-            for (int i = 0; i < objects.GetLength(0); ++i)
+            foreach(IGameObject gameObject in dynamicObjects)
             {
-                for (int j = 0; j < objects.GetLength(1); ++j)
+                gameObject.Update(time, new List<IGameObject>());
+            }
+
+            for (int i = 0; i < stationaryObjects.GetLength(0); ++i)
+            {
+                for (int j = 0; j < stationaryObjects.GetLength(1); ++j)
                 {
-                    if (objects[i, j] != null)
+                    if (stationaryObjects[i, j] != null)
                     {
                         // note: the list will contain all objects that are possible to collide with objects[i, j]
                         // BaseGameObject.OnCollision may be triggered multiple times (actually, it is a status instead of an event)
-                        objects[i, j].Update(time, new List<IGameObject>());
+                        stationaryObjects[i, j].Update(time, new List<IGameObject>());
                     }
                 }
             }
@@ -80,13 +87,18 @@ namespace MelloMario
 
         internal void Draw(GameTime time, SpriteBatch spriteBatch)
         {
-            for (int i = 0; i < objects.GetLength(0); ++i)
+            foreach (IGameObject gameObject in dynamicObjects)
             {
-                for (int j = 0; j < objects.GetLength(1); ++j)
+                gameObject.Draw(time, spriteBatch);
+            }
+
+            for (int i = 0; i < stationaryObjects.GetLength(0); ++i)
+            {
+                for (int j = 0; j < stationaryObjects.GetLength(1); ++j)
                 {
-                    if (objects[i, j] != null)
+                    if (stationaryObjects[i, j] != null)
                     {
-                        objects[i, j].Draw(time, spriteBatch);
+                        stationaryObjects[i, j].Draw(time, spriteBatch);
                     }
                 }
             }
