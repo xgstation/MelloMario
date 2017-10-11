6435660f474f1e8e09772f0008a65c2371c3e09a
hcz <h.cz@qq.com>
2017-10-11 00:26:27 -0400

    Add interface IGameCharacter.

---
+++ MelloMario/MelloMario/Interfaces/IGameCharacter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MelloMario
+{
+    interface IGameCharacter
+    {
+        void Left();
+        void Right();
+        void Jump();
+        void Crouch();
+        void Action();
+    }
+}
