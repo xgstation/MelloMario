b07a61c47c6aa9a8e130c395150ed314598e1a63
hcz <h.cz@qq.com>
2017-10-14 01:23:50 -0400

    Apply changes.

--- MelloMario/MelloMario/Theming/GameScript.cs
+++ MelloMario/MelloMario/Theming/GameScript.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
-using MelloMario.Controllers;
-using MelloMario.MarioObjects;
 using MelloMario.Factories;
 
 namespace MelloMario
@@ -12,35 +10,34 @@ namespace MelloMario
         {
         }
 
-        public void Bind(IEnumerable<IController> controllers, Mario mario)
+        public void Bind(IEnumerable<IController> controllers, IGameCharacter character)
         {
             ICommandFactory factory = CommandFactory.Instance;
 
             foreach (IController controller in controllers)
             {
                 //controller.AddCommand(Keys.Escape, factory.CreateGameModelCommand("Pause", objects));
-                //controller.AddCommand(Keys.Down, factory.CreateGameCharacterCommand("Fall", mario), KeyBehavior.hold);
-                controller.AddCommand(Keys.Space, factory.CreateGameCharacterCommand("Action", mario));
-                controller.AddCommand(Keys.Down, factory.CreateGameCharacterCommand("Crouch", mario), KeyBehavior.hold);
-                controller.AddCommand(Keys.Up, factory.CreateGameCharacterCommand("Jump", mario), KeyBehavior.hold);
-                controller.AddCommand(Keys.Left, factory.CreateGameCharacterCommand("Left", mario), KeyBehavior.hold);
-                controller.AddCommand(Keys.Right, factory.CreateGameCharacterCommand("Right", mario), KeyBehavior.hold);
+                controller.AddCommand(Keys.Space, factory.CreateGameCharacterCommand("Action", character));
+                controller.AddCommand(Keys.Down, factory.CreateGameCharacterCommand("Crouch", character), KeyBehavior.hold);
+                controller.AddCommand(Keys.Up, factory.CreateGameCharacterCommand("Jump", character), KeyBehavior.hold);
+                controller.AddCommand(Keys.Left, factory.CreateGameCharacterCommand("Left", character), KeyBehavior.hold);
+                controller.AddCommand(Keys.Right, factory.CreateGameCharacterCommand("Right", character), KeyBehavior.hold);
 
-                controller.AddCommand(Keys.S, factory.CreateGameCharacterCommand("Crouch", mario), KeyBehavior.hold);
-                controller.AddCommand(Keys.W, factory.CreateGameCharacterCommand("Jump", mario), KeyBehavior.hold);
-                controller.AddCommand(Keys.A, factory.CreateGameCharacterCommand("Left", mario), KeyBehavior.hold);
-                controller.AddCommand(Keys.D, factory.CreateGameCharacterCommand("Right", mario), KeyBehavior.hold);
+                controller.AddCommand(Keys.S, factory.CreateGameCharacterCommand("Crouch", character), KeyBehavior.hold);
+                controller.AddCommand(Keys.W, factory.CreateGameCharacterCommand("Jump", character), KeyBehavior.hold);
+                controller.AddCommand(Keys.A, factory.CreateGameCharacterCommand("Left", character), KeyBehavior.hold);
+                controller.AddCommand(Keys.D, factory.CreateGameCharacterCommand("Right", character), KeyBehavior.hold);
 
-                controller.AddCommand(Keys.Y, factory.CreateGameObjectCommand("StandardState", mario));
-                controller.AddCommand(Keys.U, factory.CreateGameObjectCommand("SuperState", mario));
-                controller.AddCommand(Keys.I, factory.CreateGameObjectCommand("FireState", mario));
-                controller.AddCommand(Keys.O, factory.CreateGameObjectCommand("DeadState", mario));
+                controller.AddCommand(Keys.Y, factory.CreateGameObjectCommand("StandardState", character));
+                controller.AddCommand(Keys.U, factory.CreateGameObjectCommand("SuperState", character));
+                controller.AddCommand(Keys.I, factory.CreateGameObjectCommand("FireState", character));
+                controller.AddCommand(Keys.O, factory.CreateGameObjectCommand("DeadState", character));
 
-                controller.AddCommand(Buttons.B, factory.CreateGameCharacterCommand("Action", mario));
-                controller.AddCommand(Buttons.DPadDown, factory.CreateGameCharacterCommand("Crouch", mario), KeyBehavior.hold);
-                controller.AddCommand(Buttons.A, factory.CreateGameCharacterCommand("Jump", mario), KeyBehavior.hold);
-                controller.AddCommand(Buttons.DPadLeft, factory.CreateGameCharacterCommand("Left", mario), KeyBehavior.hold);
-                controller.AddCommand(Buttons.DPadRight, factory.CreateGameCharacterCommand("Right", mario), KeyBehavior.hold);
+                controller.AddCommand(Buttons.B, factory.CreateGameCharacterCommand("Action", character));
+                controller.AddCommand(Buttons.DPadDown, factory.CreateGameCharacterCommand("Crouch", character), KeyBehavior.hold);
+                controller.AddCommand(Buttons.A, factory.CreateGameCharacterCommand("Jump", character), KeyBehavior.hold);
+                controller.AddCommand(Buttons.DPadLeft, factory.CreateGameCharacterCommand("Left", character), KeyBehavior.hold);
+                controller.AddCommand(Buttons.DPadRight, factory.CreateGameCharacterCommand("Right", character), KeyBehavior.hold);
             }
         }
     }
