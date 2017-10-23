16009f00c48a8acb4a6e0ae9914dadfc6bb7a323
Sergey Maltsev <maltsev.1@osu.edu>
2017-10-23 11:24:24 -0400

    Star and Red mushroom movement, remove self on collision broken
    Related Work Items: #138, #139

--- MelloMario/MelloMario/ItemObjects/Star.cs
+++ MelloMario/MelloMario/ItemObjects/Star.cs
@@ -5,9 +5,10 @@ using MelloMario.MarioObjects;
 
 namespace MelloMario.ItemObjects
 {
-    class Star : BaseGameObject
+    class Star : BasePhysicalObject
     {
         private IItemState state;
+        private bool goingRight;
 
         private void UpdateSprite()
         {
@@ -16,14 +17,47 @@ namespace MelloMario.ItemObjects
 
         protected override void OnSimulation(GameTime time)
         {
+
+            ApplyGravity();
             state.Update(time);
+            if (state is Normal)
+            {
+                if (goingRight)
+                    Move(new Point(3, 0));
+                else
+                    Move(new Point(-3, 0));
+            }
+            base.OnSimulation(time);
         }
 
         protected override void OnCollision(IGameObject target, CollisionMode mode, CollisionCornerMode corner, CollisionCornerMode cornerPassive)
         {
-            if (target is Mario && state is Normal)
+            switch (target.GetType().Name)
             {
-                Collect();
+                case "Mario":
+                    if (state is Normal)
+                        Collect();
+                    break;
+                case "Brick":
+                case "Question":
+                case "Floor":
+                case "Pipeline":
+                case "Stair":
+                    // TODO: check against hidden
+                    Bounce(mode, new Vector2());
+                    if (mode == CollisionMode.Left || mode == CollisionMode.InnerLeft && corner == CollisionCornerMode.Center)
+                    {
+                        Bounce(mode, new Vector2(), 1);
+                        goingRight = true;
+                    }
+                    else if (mode == CollisionMode.Right || mode == CollisionMode.InnerRight && corner == CollisionCornerMode.Center)
+                    {
+                        Bounce(mode, new Vector2(), 1);
+                        goingRight = false;
+                    }
+                    if (mode == CollisionMode.Bottom || mode == CollisionMode.InnerBottom && corner == CollisionCornerMode.Center)
+                        ApplyForce(new Vector2(0, -160f));
+                    break;
             }
         }
 
@@ -48,8 +82,9 @@ namespace MelloMario.ItemObjects
             }
         }
 
-        public Star(IGameWorld world, Point location, bool isUnveil) : base(world, location, new Point(32, 32))
+        public Star(IGameWorld world, Point location, bool isUnveil) : base(world, location, new Point(32, 32), 32)
         {
+            goingRight = true;
             if (isUnveil)
             {
                 state = new Unveil(this);
--- MelloMario/MelloMario/ItemObjects/SuperMushroom.cs
+++ MelloMario/MelloMario/ItemObjects/SuperMushroom.cs
@@ -2,12 +2,14 @@
 using MelloMario.Factories;
 using MelloMario.ItemObjects.SuperMushroomStates;
 using MelloMario.MarioObjects;
+using MelloMario.BlockObjects;
 
 namespace MelloMario.ItemObjects
 {
-    class SuperMushroom : BaseGameObject
+    class SuperMushroom : BasePhysicalObject
     {
         private IItemState state;
+        private bool goingRight;
 
         private void UpdateSprite()
         {
@@ -16,14 +18,45 @@ namespace MelloMario.ItemObjects
 
         protected override void OnSimulation(GameTime time)
         {
+            
+            ApplyGravity();
             state.Update(time);
+            if (state is Normal)
+            {
+                if (goingRight)
+                    Move(new Point(1,0));
+                else
+                    Move(new Point(-1, 0));
+            }
+            base.OnSimulation(time);
         }
 
         protected override void OnCollision(IGameObject target, CollisionMode mode, CollisionCornerMode corner, CollisionCornerMode cornerPassive)
         {
-            if (target is Mario && state is Normal)
+            switch(target.GetType().Name)
             {
-                Collect();
+                case "Mario":
+                    if (state is Normal)
+                        Collect();
+                    break;
+                case "Brick":
+                case "Question":
+                case "Floor":
+                case "Pipeline":
+                case "Stair":
+                    // TODO: check against hidden
+                    Bounce(mode, new Vector2());
+                    if (mode == CollisionMode.Left || mode == CollisionMode.InnerLeft && corner == CollisionCornerMode.Center)
+                    {
+                        Bounce(mode, new Vector2(), 1);
+                        goingRight = true;
+                    }
+                    else if (mode == CollisionMode.Right || mode == CollisionMode.InnerRight && corner == CollisionCornerMode.Center)
+                    {
+                        Bounce(mode, new Vector2(), 1);
+                        goingRight = false;
+                    }
+                    break;
             }
         }
 
@@ -48,8 +81,9 @@ namespace MelloMario.ItemObjects
         {
         }
 
-        public SuperMushroom(IGameWorld world, Point location, bool isUnveil) : base(world, location, new Point(32, 32))
+        public SuperMushroom(IGameWorld world, Point location, bool isUnveil) : base(world, location, new Point(32, 32), 32)
         {
+            goingRight = true;
             if (isUnveil)
             {
                 state = new Unveil(this);
