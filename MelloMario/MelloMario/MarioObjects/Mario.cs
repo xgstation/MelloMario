0b951c4838d23388ab379c6e0c4054569059fd7e
Sergey Maltsev <maltsev.1@osu.edu>
2017-10-31 14:27:03 -0400

    slightly increased max jump height, fixed super mario acting as a down grade

--- MelloMario/MelloMario/MarioObjects/Mario.cs
+++ MelloMario/MelloMario/MarioObjects/Mario.cs
@@ -174,7 +174,8 @@ namespace MelloMario.MarioObjects
                         ProtectionState.Star();
                     break;
                 case "SuperMushroom":
-                    if (((ItemObjects.SuperMushroom)target).State is ItemObjects.SuperMushroomStates.Normal)
+                    if (((ItemObjects.SuperMushroom)target).State is ItemObjects.SuperMushroomStates.Normal &&
+                        PowerUpState is Standard)
                         PowerUpState.UpgradeToSuper();
                     break;
             }
