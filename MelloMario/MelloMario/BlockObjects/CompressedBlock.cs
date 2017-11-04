7dc6eb90819cd0dbe5afaefc3cfa47102dbe6957
Gen Xu <xu.2289@osu.edu>
2017-11-04 05:18:09 -0400

    New class added: Allow combine multiple objects into one to save resource, still WIP.

---
+++ MelloMario/MelloMario/BlockObjects/CompressedBlock.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelloMario.Factories;
+using MelloMario.Sprites;
+using Microsoft.Xna.Framework;
+
+namespace MelloMario.BlockObjects
+{
+    class CompressedBlock : BaseCollidableObject
+    {
+        public CompressedBlock(IGameWorld world, Point location, Point fullSize, Type type) : base(world, location, fullSize)
+        {
+            ShowSprite(SpriteFactory.Instance.CreateCompressedSprite(fullSize, type.Name));
+        }
+
+        protected override void OnUpdate(GameTime time)
+        {
+        }
+
+        protected override void OnDraw(GameTime time, Rectangle viewport, ZIndex zIndex)
+        {
+        }
+
+        protected override void OnCollision(IGameObject target, CollisionMode mode, CornerMode corner, CornerMode cornerPassive)
+        {
+        }
+
+        protected override void OnSeen(IPlayer player, CollisionMode mode)
+        {
+        }
+
+        protected override void OnOut(CollisionMode mode)
+        {
+        }
+    }
+}
