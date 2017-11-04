d9aaeaa2c170db3ea46fe67ac0f939ca4da4f837
Gen Xu <xu.2289@osu.edu>
2017-11-04 06:34:29 -0400

    New enemy class added, to be finished.

---
+++ MelloMario/MelloMario/EnemyObjects/Piranha.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace MelloMario.EnemyObjects
+{
+    class Piranha : BasePhysicalObject
+    {
+        private float hideTime;
+        public Piranha(IGameWorld world, Point location, Point size, float hideTime, float pixelScale) : base(world, location, size, pixelScale)
+        {
+            this.hideTime = hideTime;
+            //TODO: Implment Piranha sprite, factory method and update interface.
+            //ShowSprite(Factories.SpriteFactory.Instance.createPiranhaSprite());
+        }
+
+        protected override void OnUpdate(GameTime time)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnDraw(GameTime time, Rectangle viewport, ZIndex zIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnCollision(IGameObject target, CollisionMode mode, CornerMode corner, CornerMode cornerPassive)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnSeen(IPlayer player, CollisionMode mode)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnOut(CollisionMode mode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
