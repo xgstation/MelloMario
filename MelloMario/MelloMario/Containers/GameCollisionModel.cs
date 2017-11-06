8a28981bc1d69a248354bf53e16b5b7e32895a87
Gen Xu <xu.2289@osu.edu>
2017-11-06 02:50:42 -0500

    WIP

---
+++ MelloMario/MelloMario/Containers/GameCollisionModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelloMario.MarioObjects;
+
+namespace MelloMario.Containers
+{
+    //Work in progress
+    class GameCollisionModel
+    {
+        public enum CollisionResponse { Attack, Bounce, Obstacle, Squeeze}
+        private ISet<ICharacter> characterSet;
+        private GameWorld world;
+
+        void UpdateCharacterCollision(ICharacter character)
+        {
+            var mario = character.GetType().IsAssignableFrom(typeof(PlayerMario)) ? character as PlayerMario : null;
+            if (mario == null) return;;
+            var nearByObjects = world.ScanNearby(mario.Viewport);
+            foreach (var nearByObj in nearByObjects)
+            {
+                if (nearByObj.Boundary.Intersects(mario.Boundary))
+                {
+                    
+                }
+            }
+        }
+
+        void UpdateEnemyCollision()
+        {
+            
+        }
+
+        void UpdateBlockCollision()
+        {
+            
+        }
+
+        void UpdateItemCollision()
+        {
+            
+        }
+    }
+}
