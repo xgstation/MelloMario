5602b8728e82d0144db78a2a07d003f281ae3a47
hcz <h.cz@qq.com>
2017-11-08 21:30:11 -0500

    Add GameSession.

---
+++ MelloMario/MelloMario/Containers/GameSession.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace MelloMario.Containers
+{
+    class GameSession : BaseContainer<IGameWorld, IPlayer>
+    {
+        protected override IGameWorld GetKey(IPlayer value)
+        {
+            return value.CurrentWorld;
+        }
+    }
+}
