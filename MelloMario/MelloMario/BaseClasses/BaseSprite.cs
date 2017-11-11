4ee643809c021a9bfd487644a91db0bd7fabec1f
Gen Xu <xu.2289@osu.edu>
2017-11-11 00:40:14 -0500

    LayerDepth Added.

--- MelloMario/MelloMario/BaseClasses/BaseSprite.cs
+++ MelloMario/MelloMario/BaseClasses/BaseSprite.cs
@@ -6,7 +6,7 @@ namespace MelloMario.Sprites
     abstract class BaseSprite : ISprite
     {
         private Point size;
-        private ZIndex activeZIndex;
+        private ZIndex zIndex;
         private bool visible;
 
         protected SpriteBatch spriteBatch;
@@ -25,26 +25,44 @@ namespace MelloMario.Sprites
                 return size;
             }
         }
-
-        public ZIndex ZIndex
+        protected float LayerDepth
         {
             get
             {
-                return activeZIndex;
+                switch (zIndex)
+                {
+                    case ZIndex.hud:
+                        return 0.05f;
+                    case ZIndex.foreground:
+                        return 0.1f;
+                    case ZIndex.level:
+                        return 0.25f;
+                    case ZIndex.item:
+                        return 0.4f;
+                    case ZIndex.background3:
+                        return 0.5f;
+                    case ZIndex.background2:
+                        return 0.6f;
+                    case ZIndex.background1:
+                        return 0.85f;
+                    case ZIndex.background0:
+                        return 0.95f;
+                    default:
+                        return 1f;
+                }
             }
         }
-
-        public BaseSprite(SpriteBatch spriteBatch, Point size, ZIndex activeZIndex)
+        public BaseSprite(SpriteBatch spriteBatch, Point size, ZIndex zIndex)
         {
             this.spriteBatch = spriteBatch;
             this.size = size;
-            this.activeZIndex = activeZIndex;
+            this.zIndex = zIndex;
             visible = true;
         }
 
-        public void Draw(int time, Rectangle destination, ZIndex zIndex)
+        public void Draw(int time, Rectangle destination)
         {
-            if (visible && zIndex == activeZIndex)
+            if (visible)
             {
                 OnDraw(time, destination);
             }
--- MelloMario/MelloMario/Sprites/TextSprite.cs
+++ MelloMario/MelloMario/Sprites/TextSprite.cs
@@ -8,13 +8,23 @@ namespace MelloMario.Sprites
         private string text;
         private SpriteFont font;
         private Color color;
-
+        
         protected override void OnDraw(int time, Rectangle destination)
         {
-            spriteBatch.DrawString(font, text, destination.Location.ToVector2(), color);
+            spriteBatch.DrawString(
+                font,
+                text,
+                destination.Location.ToVector2(),
+                color,
+                0f,//rotation
+                new Vector2(), //origin
+                1, //scale
+                SpriteEffects.None,
+                LayerDepth
+                );
         }
 
-        public TextSprite(SpriteBatch spriteBatch, string text, SpriteFont font, Point size, ZIndex activeZIndex) : base(spriteBatch, size, activeZIndex)
+        public TextSprite(SpriteBatch spriteBatch, string text, SpriteFont font, Point size, ZIndex zIndex) : base(spriteBatch, size, zIndex)
         {
             this.text = text;
             this.font = font;
