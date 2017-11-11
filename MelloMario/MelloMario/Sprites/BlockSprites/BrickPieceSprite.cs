55213cb7fb6d2e75155a481797396b0191eb4fe2
Gen Xu <xu.2289@osu.edu>
2017-11-11 02:18:26 -0500

    Physical model applied.

--- MelloMario/MelloMario/Sprites/BlockSprites/BrickPieceSprite.cs
+++ MelloMario/MelloMario/Sprites/BlockSprites/BrickPieceSprite.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using MelloMario.Theming;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Graphics.PackedVector;
 
@@ -70,34 +72,35 @@ namespace MelloMario.Sprites.BlockSprites
             float[] offsetX = new float[4];
             float[] offsetY = new float[4];
             Rectangle[] source = new Rectangle[4];
-            
+
+            float k = 5f / Math.Abs(2f - GameConst.FORCE_F_AIR/20f);
             //LB
-            offsetX[0] = -elapsed / 5f + 4f;
-            offsetY[0] = elapsed * elapsed / 400f - elapsed + 16f;
+            offsetX[0] = -elapsed / k + 4f;
+            offsetY[0] = GameConst.FORCE_G * elapsed * elapsed / 16000f - elapsed + 16f;
             destinations[0] = new Vector2(offsetX[0], offsetY[0]) + origin;
             source[0] = new Rectangle(0, 0, texture.Width / 2, texture.Height / 2);
 
             //LT
-            offsetX[1] = -elapsed / 5f + 4f;
-            offsetY[1] = elapsed * elapsed / 360f - elapsed + 32f;
+            offsetX[1] = -elapsed / k + 4f;
+            offsetY[1] = GameConst.FORCE_G * elapsed * elapsed / 14400f - elapsed + 32f;
             destinations[1] = new Vector2(offsetX[1], offsetY[1]) + origin;
             source[1] = new Rectangle(0, texture.Height / 2, texture.Width / 2, texture.Height / 2);
 
             //RB
-            offsetX[2] = elapsed / 5f + 12f;
-            offsetY[2] = elapsed * elapsed / 400f - elapsed + 16f;
+            offsetX[2] = elapsed / k + 12f;
+            offsetY[2] = GameConst.FORCE_G * elapsed * elapsed / 16000f - elapsed + 16f;
             destinations[2] = new Vector2(offsetX[2], offsetY[2]) + origin;
             source[2] = new Rectangle(texture.Width / 2, 0, texture.Width / 2, texture.Height / 2);
 
             //RT
-            offsetX[3] = elapsed / 5f + 12f;
-            offsetY[3] = elapsed * elapsed / 360f - elapsed + 32f;
+            offsetX[3] = elapsed / k + 12f;
+            offsetY[3] = GameConst.FORCE_G * elapsed * elapsed / 14400f - elapsed + 32f;
             destinations[3] = new Vector2(offsetX[3], offsetY[3]) + origin;
             source[3] = new Rectangle(texture.Width / 2, texture.Height / 2, texture.Width / 2, texture.Height / 2);
 
             var spriteOrigin = new Vector2(8f, 8f);
             var scale = new Vector2(1.25f, 1.25f);
-            var rotation = elapsed * 1.5f;
+            var rotation = elapsed * 30f / GameConst.FORCE_F_AIR;
             spriteBatch.Draw(texture, destinations[0], source[0], Color.White, rotation, spriteOrigin, scale, SpriteEffects.None, LayerDepth);
             spriteBatch.Draw(texture, destinations[1], source[1], Color.White, rotation, spriteOrigin, scale, SpriteEffects.None, LayerDepth);
             spriteBatch.Draw(texture, destinations[2], source[2], Color.White, -rotation, spriteOrigin, scale, SpriteEffects.None, LayerDepth);
