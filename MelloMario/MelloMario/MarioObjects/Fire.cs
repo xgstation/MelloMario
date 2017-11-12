499c3e583919d7e4635a99daabeeb3379e2e50d9
Gen Xu <xu.2289@osu.edu>
2017-11-12 16:41:34 -0500

    Fire class implemented.

---
+++ MelloMario/MelloMario/MarioObjects/Fire.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelloMario.Factories;
+using Microsoft.Xna.Framework;
+
+namespace MelloMario.MarioObjects
+{
+    class Fire : BasePhysicalObject
+    {
+        protected override void OnSimulation(int time)
+        {
+            base.OnSimulation(time);
+        }
+
+        protected override void OnUpdate(int time)
+        {
+            //throw new NotImplementedException();
+        }
+
+        protected override void OnDraw(int time, Rectangle viewport)
+        {
+            //throw new NotImplementedException();
+        }
+
+        protected override void OnCollision(IGameObject target, CollisionMode mode, CornerMode corner, CornerMode cornerPassive)
+        {
+            if (target is Mario)
+            {
+                return;
+            }
+        }
+
+        protected override void OnCollideViewport(IPlayer player, CollisionMode mode)
+        {
+            //throw new NotImplementedException();
+        }
+
+        protected override void OnCollideWorld(CollisionMode mode)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public Fire(IGameWorld world, Point location, bool isRight = true) : base(world, location, new Point(16, 16), 32, 5f)
+        {
+            ShowSprite(SpriteFactory.Instance.CreateFireSprite());
+        }
+
+    }
+}
