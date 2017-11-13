8c260d616236f60ca1f59957017bc1a0fc90e533
hcz <h.cz@qq.com>
2017-11-13 11:40:14 -0500

    PlayerMario should not contain QuadTree's implementation detail.

--- MelloMario/MelloMario/Collision/EncapsulatedQuadTreeObject.cs
+++ MelloMario/MelloMario/Collision/EncapsulatedQuadTreeObject.cs
@@ -10,7 +10,18 @@ namespace MelloMario.Collision
         {
             realObject = obj;
         }
-        public Rectangle Boundary { get { return realObject.Boundary; } }
+        public Rectangle Boundary { get {
+            Rectangle b = realObject.Boundary;
+            if (b.Location.X < 0)
+            {
+                b.Location.X = 0;
+            }
+            if (b.Location.Y < 0)
+            {
+                b.Location.Y = 0;
+            }
+            return b;
+        } }
         public QuadTreeNode<T> Owner { get { return owner; } set { owner = value; } }
         public T realObj { get { return realObject; } }
     }
