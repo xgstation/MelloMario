537515fae9dad845090fc180bc3fa67c11da78bd
Gen Xu <xu.2289@osu.edu>
2017-11-20 06:18:50 -0500

    Updated deserializer with new property of world.

--- MelloMario/MelloMario/LevelGen/GameConverter.cs
+++ MelloMario/MelloMario/LevelGen/GameConverter.cs
@@ -79,7 +79,7 @@ namespace MelloMario.LevelGen
 
             Util.TryGet(out Point initialPoint, mapToBeLoaded, "InitialSpawnPoint");
             Util.TryGet(out IList<Point> respawnPoints, mapToBeLoaded, "RespawnPoints");
-            world = new World(index, mapSize, new Point(2, 2), respawnPoints);
+            world = new World(index, mapType, mapSize, new Point(2, 2), respawnPoints);
 
             gameEntityConverter = new GameEntityConverter(model, world, listener);
 
