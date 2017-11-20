cfe3e97070e72ef8f5fd83cad5b93ee1e381217c
hcz <h.cz@qq.com>
2017-11-20 17:14:11 -0500

    Implement SoundTrack.

---
+++ MelloMario/MelloMario/Sounds/SoundTrack.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Media;
+
+namespace MelloMario.Sounds
+{
+    internal class SoundTrack : ISoundTrack
+    {
+        private Song instance;
+
+        public SoundTrack(Song instance)
+        {
+            this.instance = instance;
+        }
+
+        public void Play()
+        {
+            // instance.();
+        }
+    }
+}
