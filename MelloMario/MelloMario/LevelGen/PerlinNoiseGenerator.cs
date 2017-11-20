ee4a21c6a52c3ea835e17742eaea54261a7f990e
Gen Xu <xu.2289@osu.edu>
2017-11-19 20:38:43 -0500

    Perlin Noise generator finished.

--- MelloMario/MelloMario/LevelGen/PerlinNoiseGenerator.cs
+++ MelloMario/MelloMario/LevelGen/PerlinNoiseGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Security.Cryptography;
 using Microsoft.Xna.Framework;
 
@@ -10,74 +11,81 @@ namespace MelloMario.LevelGen
         private static readonly RNGCryptoServiceProvider RngCrypto = new RNGCryptoServiceProvider();
         private static int Size;
 
-        private static int[] PermuteTable =
-        {
-            151, 160, 137, 91, 90, 15,  131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23,
-            190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237,
-            149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231,
-            83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161,
-            1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109,
-            198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206,
-            59, 227, 47, 16, 58, 17, 182, 189, 28, 42, 223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153,
-            101, 155, 167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218,
-            246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107,
-            49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205,
-            93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180,151, 160, 137, 91, 90, 15,  131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23,
-            190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237,
-            149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231,
-            83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161,
-            1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109,
-            198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206,
-            59, 227, 47, 16, 58, 17, 182, 189, 28, 42, 223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153,
-            101, 155, 167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218,
-            246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107,
-            49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205,
-            93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180
-        };
+        private static int[] PermuteTable;
 
-        public PerlinNoiseGenerator(int size)
+        private static readonly byte[] GradSeed = new byte[4];
+
+        public PerlinNoiseGenerator(int newSize = 256)
         {
-            Size = size;
-            //InitializeNormal();
+            Size = newSize;
+            RngCrypto.GetBytes(GradSeed);
+            InitializePermuteTable();
         }
-        
-        public void InitializeNormal()
+
+        private static void Swap(ref int[] array, int i, int j)
         {
-            PermuteTable = new int[Size * 2];
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+        private static void Shuffle()
+        {
+            byte[] bytes = new byte[4];
             for (int i = 0; i < Size * 2; i++)
             {
-                byte[] bytes = new byte[4];
                 RngCrypto.GetBytes(bytes);
-                PermuteTable[i] = BitConverter.ToInt32(bytes, 0) % Size;
+                int k = Math.Abs(BitConverter.ToInt32(bytes, 0)) % Size;
+                RngCrypto.GetBytes(bytes);
+                int l = Math.Abs(BitConverter.ToInt32(bytes, 0)) % Size;
+                Swap(ref PermuteTable, k, l);
             }
         }
 
-        public float Perlin(Vector2 loc)
+        private static void InitializePermuteTable()
         {
-            float x = loc.X;
-            float y = loc.Y;
-            int vx = (int)x % Size;
-            int vy = (int)y % Size;
-            float rx = x - (int)x;
-            float ry = y - (int)y;
-            float fx = Fade(rx);
-            float fy = Fade(ry);
-
-            int hash1 = PermuteTable[PermuteTable[vx] + vy];
-            int hash2 = PermuteTable[PermuteTable[vx] + vy + 1];
-            int hash3 = PermuteTable[PermuteTable[vx + 1] + vy];
-            int hash4 = PermuteTable[PermuteTable[vx + 1] + vy + 1];
-
-
-            float u = Lerp(Grad(hash1, rx, ry), Grad(hash3, rx - 1, ry), fx);
-            float v = Lerp(Grad(hash2, rx, ry - 1), Grad(hash4, rx, ry), fy);
-
-            return (u + v) / 2;
+            PermuteTable = new int[Size * 2];
+            for (int i = 0; i < Size; i++)
+            {
+                PermuteTable[i] = i;
+            }
+            Shuffle();
+            Array.Copy(PermuteTable, 0, PermuteTable, 256, 256);
         }
 
-        private static float Grad(int hash, float x, float y)
+        public float Noise(Vector2 p)
         {
-            switch (hash & 0x3)
+            float a = Perlin(p) + 0.5f * Perlin(p) + 0.25f * Perlin(4f * p) + 0.125f * Perlin(8f * p);
+            return a;
+        }
+
+        public float Perlin(Vector2 p)
+        {
+            Point pi = p.ToPoint();
+            pi.X %= (Size - 1);
+            pi.Y %= (Size - 1);
+
+            Vector2 vf = p - p.ToPoint().ToVector2();
+            Vector2 uf = new Vector2(Fade(vf.X), Fade(vf.Y));
+
+            int hash1 = PermuteTable[PermuteTable[pi.X] + pi.Y];
+            int hash2 = PermuteTable[PermuteTable[pi.X] + pi.Y + 1];
+            int hash3 = PermuteTable[PermuteTable[pi.X + 1] + pi.Y];
+            int hash4 = PermuteTable[PermuteTable[pi.X + 1] + pi.Y + 1];
+
+            float ga = GradContribute(hash1, vf);
+            float gb = GradContribute(hash2, vf - new Vector2(0, 1));
+            float gc = GradContribute(hash3, vf - new Vector2(1, 0));
+            float gd = GradContribute(hash4, vf - new Vector2(1, 1));
+
+            return Lerp(Lerp(ga, gc, uf.X), Lerp(gb, gd, uf.X), uf.Y);
+        }
+        private static float GradContribute(int hash, Vector2 v)
+        {
+
+            float x = BitConverter.ToInt32(GradSeed, 0) / (float)int.MaxValue * 0 + v.X;
+            float y = BitConverter.ToInt32(GradSeed, 0) / (float)int.MaxValue * 0 + v.Y;
+            switch (hash & 0x7)
             {
                 case 0x0:
                     return x + y;
@@ -87,18 +95,31 @@ namespace MelloMario.LevelGen
                     return x - y;
                 case 0x3:
                     return -x - y;
-                default: return 0;
+                case 0x4:
+                    return x;
+                case 0x5:
+                    return y;
+                case 0x6:
+                    return -x;
+                case 0x7:
+                    return -y;
+                default:
+                    return 0;
             }
         }
 
         private static float Fade(float f)
         {
-            return (float)(6 * Math.Pow(f, 5) - 15 * Math.Pow(f, 4) + 10 * Math.Pow(f, 3));
+            //return (float)(6 * Math.Pow(f, 5) - 15 * Math.Pow(f, 4) + 10 * Math.Pow(f, 3));
+            //replaced with optimized and equivalent version below
+            return f * f * f * (f * (f * 6 - 15) + 10);
         }
 
         private static float Lerp(float a, float b, float k)
         {
-            return a + k * (b - a);
+            return a + (b - a) * k;
+            //TODO: Determine which is faster
+            //return MathHelper.Lerp(a, b, k);
         }
     }
 }
