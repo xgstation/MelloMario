20985501fe16a973b6ac7c8c0f27c27da7c0ec39
Gen Xu <xu.2289@osu.edu>
2017-11-20 19:16:19 -0500

    Sound even listener added.

---
+++ MelloMario/MelloMario/Sounds/SoundListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelloMario.Factories;
+using MelloMario.Objects.Items;
+
+namespace MelloMario.Sounds
+{
+    internal class SoundListener : IListener<ISoundable>
+    {
+        public void Subscribe(ISoundable soundObject)
+        {
+            soundObject.SoundEvent += Sound;
+        }
+
+        private static void Sound(ISoundable c, EventArgs e)
+        {
+            switch (c)
+            {
+                case Coin coin:
+                    SoundFactory.Instance.CreateSoundEffect("Coin");
+                    break;
+            }
+        }
+    }
+}
