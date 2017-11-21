be55295eded34db6f2433a141c75d719c4727e8b
hcz <h.cz@qq.com>
2017-11-21 16:16:42 -0500

    Add JsonGenerator.

---
+++ MelloMario/MelloMario/LevelGen/JsonGenerator.cs
@@ -0,0 +1,21 @@
+﻿namespace MelloMario.LevelGen
+{
+    #region
+
+    using System;
+    using System.Collections.Generic;
+    using System.Security.Cryptography;
+    using MelloMario.Objects.Blocks;
+    using MelloMario.Objects.Enemies;
+    using Microsoft.Xna.Framework;
+
+    #endregion
+
+    internal class JsonGenerator : StaticGenerator
+    {
+        public JsonGenerator(IEnumerable<IGameObject> objects)
+        {
+            this.objects = objects;
+        }
+    }
+}
