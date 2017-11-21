e5db112ea6c788b18122ec4da499e1ba20fd7b46
hcz <h.cz@qq.com>
2017-11-21 15:12:18 -0500

    Implement GroundGenerator.Request.

--- MelloMario/MelloMario/LevelGen/GroundGenerator.cs
+++ MelloMario/MelloMario/LevelGen/GroundGenerator.cs
@@ -28,15 +28,20 @@
 
         public void Request(Rectangle range, IWorld world)
         {
-            //float temp = camera.Viewport.Location.X + 800 + 32;
-            //if ((int) temp / 32 >= (int) rightMostReachedX / 32)
-            //{
-            //    difficultyIndex += double.Epsilon;
-            //    new Floor(world, new Point(world.Boundary.Right, 13 * 32), listener);
-            //    new Floor(world, new Point(world.Boundary.Right, 14 * 32), listener);
-            //    world.Extend(0, 32, 0, 0);
-            //    rightMostReachedX += 32;
-            //}
+            // note: top / buttom are locked
+
+            while (world.Boundary.Right < range.Right)
+            {
+                new Floor(world, new Point(world.Boundary.Right, 13 * 32), listener);
+                new Floor(world, new Point(world.Boundary.Right, 14 * 32), listener);
+                world.Extend(0, 32, 0, 0);
+            }
+            while (world.Boundary.Left > range.Left)
+            {
+                new Floor(world, new Point(world.Boundary.Left - 32, 13 * 32), listener);
+                new Floor(world, new Point(world.Boundary.Left - 32, 14 * 32), listener);
+                world.Extend(32, 0, 0, 0);
+            }
         }
     }
 }
