6bc00e171dd0455a5d71dd4fea1cfbcf11c770bd
Gen Xu <xu.2289@osu.edu>
2017-11-26 22:42:20 -0500

    Screen State added.

---
+++ MelloMario/MelloMario/Interfaces/Objects/States/IScreenState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MelloMario
+{
+    internal interface IScreenState : IState
+    {
+        void OnGameStart();
+        void OnGameProgress();
+        void OnGamePause();
+        void OnGameOver();
+        void OnGameWon();
+    }
+}
