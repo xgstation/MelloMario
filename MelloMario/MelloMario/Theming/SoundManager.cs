097c38956078a2044b25cf18a074023e31274242
Gen Xu <xu.2289@osu.edu>
2017-11-28 14:06:25 -0500

    Sound manager added to take charge of all sound play stuff.

---
+++ MelloMario/MelloMario/Theming/SoundManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MelloMario.Theming
+{
+    using MelloMario.Sounds;
+
+    internal class SoundManager
+    {
+        public readonly IListener<ISoundable> SoundEffectListener;
+        private readonly BGMManager sounds;
+        private IModel model;
+
+        public SoundManager()
+        {
+            SoundEffectListener = new SoundEffectListener();
+            //sounds = new BGMManager();
+        }
+
+        public void BindModel(IModel newGameModel)
+        {
+            model = newGameModel;
+        }
+        public void ToggleMute()
+        {
+            sounds?.ToggleMute();
+            (SoundEffectListener as SoundEffectListener)?.ToggleMute();
+        }
+
+        public void Update(int time)
+        {
+
+        }
+
+        public void Initialize()
+        {
+            
+        }
+    }
+}
