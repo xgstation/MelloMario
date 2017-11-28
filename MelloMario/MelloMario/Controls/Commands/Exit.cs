e44a61d7da6202a8486d0d7d30a80d2ab3807bd9
Gen Xu <xu.2289@osu.edu>
2017-11-28 14:05:17 -0500

    Renamed. Receiver changed to Game class.

---
+++ MelloMario/MelloMario/Controls/Commands/Exit.cs
@@ -0,0 +1,14 @@
+﻿namespace MelloMario.Controls.Commands
+{
+    internal class Exit : BaseCommand<Game1>
+    {
+        public Exit(Game1 game) : base(game)
+        {
+        }
+
+        public override void Execute()
+        {
+            Receiver.Exit();
+        }
+    }
+}
---
+++ MelloMario/MelloMario/Controls/Commands/ToggleMute.cs
@@ -0,0 +1,14 @@
+﻿namespace MelloMario.Controls.Commands
+{
+    internal class ToggleMute : BaseCommand<Game1>
+    {
+        public ToggleMute(Game1 game) : base(game)
+        {
+        }
+
+        public override void Execute()
+        {
+            Receiver.ToggleMute();
+        }
+    }
+}
