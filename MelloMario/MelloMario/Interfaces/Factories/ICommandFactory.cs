f731295132c6f66a01fa19eb46ad415f5f1f0e6f
Gen Xu <xu.2289@osu.edu>
2017-11-28 14:00:32 -0500

    Added new method for Game class.

--- MelloMario/MelloMario/Interfaces/Factories/ICommandFactory.cs
+++ MelloMario/MelloMario/Interfaces/Factories/ICommandFactory.cs
@@ -2,6 +2,7 @@
 {
     internal interface ICommandFactory
     {
+        ICommand CreateGameCommand(string action, Game1 game);
         ICommand CreateModelCommand(string action, IModel model);
         ICommand CreateCharacterCommand(string action, ICharacter character);
     }
