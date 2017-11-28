fb26552847726ffc7f84f85ea3cd8d16c3307908
Gen Xu <xu.2289@osu.edu>
2017-11-28 13:59:40 -0500

    Added new commands factory method for Game.

--- MelloMario/MelloMario/Factories/CommandFactory.cs
+++ MelloMario/MelloMario/Factories/CommandFactory.cs
@@ -2,8 +2,9 @@
 {
     #region
 
-    using System.Diagnostics.CodeAnalysis;
     using MelloMario.Controls.Commands;
+    using System;
+    using System.Diagnostics.CodeAnalysis;
 
     #endregion
 
@@ -15,26 +16,37 @@
 
         public static ICommandFactory Instance { get; } = new CommandFactory();
 
+        public ICommand CreateGameCommand(string action, Game1 game)
+        {
+            switch (action)
+            {
+                case "CursorUp":
+                    return new CursorUp(game);
+                case "CursorDown":
+                    return new CursorDown(game);
+                case "Select":
+                    return new Select(game);
+                case "Exit":
+                    return new Exit(game);
+                case "ToggleFullScreen":
+                    return new ToggleFullScreen(game);
+                case "ToggleMute":
+                    return new ToggleMute(game);
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
         public ICommand CreateModelCommand(string action, IModel model)
         {
             switch (action)
             {
-                case "ToggleFullScreen":
-                    return new ToggleFullScreen(model);
                 case "Pause":
                     return new Pause(model);
                 case "Resume":
                     return new Resume(model);
                 case "Reset":
                     return new Reset(model);
-                case "Quit":
-                    return new Quit(model);
-                case "ToggleMute":
-                    return new Mute(model);
-                case "Normal":
-                    return new NormalMode(model);
-                case "Infinite":
-                    return new InfiniteMode(model);
                 default:
                     //it should never hit this case, if it does there is an error somewhere
                     //else in the code
@@ -48,7 +60,7 @@
             switch (action)
             {
                 case "Action":
-                    return new Action(character);
+                    return new Controls.Commands.Action(character);
                 case "Crouch":
                     return new Crouch(character);
                 case "CrouchPress":
