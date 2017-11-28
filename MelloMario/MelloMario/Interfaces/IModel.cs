a17404fbf22c328d60fe0b84b163f6c3822d43f1
Gen Xu <xu.2289@osu.edu>
2017-11-28 13:57:57 -0500

    Removed several functions into Game class.

--- MelloMario/MelloMario/Interfaces/IModel.cs
+++ MelloMario/MelloMario/Interfaces/IModel.cs
@@ -5,22 +5,29 @@
     using Microsoft.Xna.Framework.Graphics;
 
     #endregion
-
+    internal enum GameState
+    {
+        Start,
+        GameOver,
+        GameWon,
+        Pause,
+        OnProgress,
+        Transist
+    }
     internal interface IModel
     {
+         GameState State { get; }
         IPlayer ActivePlayer { get; }
         bool IsPaused { get; }
         void ToggleFullScreen();
         void Pause();
         void Resume();
-        void Init();
+        void Initialize();
         void Reset();
         void Quit();
         void Update(int time);
         void Infinite();
         void Normal();
-        void Draw(int time, SpriteBatch spriteBatch);
-        void ToggleMute();
         IWorld LoadLevel(string index);
         void TransistGameWon();
         void Transist();
