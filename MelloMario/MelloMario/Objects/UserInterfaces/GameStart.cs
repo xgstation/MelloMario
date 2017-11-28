1a13eb70e595f93a5c9ba2f526320d17a2cc22e9
Gen Xu <xu.2289@osu.edu>
2017-11-28 14:01:26 -0500

    Added dynamic menu cursor for start page.

--- MelloMario/MelloMario/Objects/UserInterfaces/GameStart.cs
+++ MelloMario/MelloMario/Objects/UserInterfaces/GameStart.cs
@@ -2,6 +2,9 @@
 {
     #region
 
+    using System;
+    using System.Diagnostics.Contracts;
+    using System.Text;
     using MelloMario.Factories;
     using Microsoft.Xna.Framework;
     using Microsoft.Xna.Framework.Graphics;
@@ -10,29 +13,67 @@
 
     internal class GameStart : BaseUIObject
     {
+        private readonly Game1 game;
         private readonly ISprite startSprite;
-        private readonly ISprite textSprite;
+        private readonly ISprite titleSprite;
+        private ISprite menuSprite;
         private readonly Rectangle startDestinationRect;
-        private readonly Rectangle textDestinationRect;
-
-        //Start splash has no relation with offset, do not use it!
-        public GameStart() : base(Point.Zero)
+        private readonly Rectangle titleDestinationRect;
+        private readonly Rectangle menuDestinationRect;
+        private readonly string[] menuStrings;
+        
+        public GameStart(Game1 game)
         {
+            this.game = game;
             startSprite = SpriteFactory.Instance.CreateTitle(ZIndex.Hud);
-            textSprite = SpriteFactory.Instance.CreateTextSprite(
-                "Mellop\n" + "Press A to play normal mode.\n" + "Press B to play infinite mode.");
+            titleSprite = SpriteFactory.Instance.CreateTextSprite(
+                "Mellop\n");
             startDestinationRect = new Rectangle(100, 100, 352, 176);
-            textDestinationRect = new Rectangle(0, 400, 200, 200);
+            titleDestinationRect = new Rectangle(0, 400, 200, 200);
+
+            menuStrings = new[]
+            {
+                "Normal Mode",
+                "Infinite Mode",
+                "Quit"
+            };
+            menuSprite = SpriteFactory.Instance.CreateTextSprite(AddCursor(0));
+            menuDestinationRect = new Rectangle(0, 450, 200, 200);
         }
         
         protected override void OnUpdate(int time)
         {
+            menuSprite = SpriteFactory.Instance.CreateTextSprite(AddCursor((int)game.CurrentSelected));
         }
 
         protected override void OnDraw(int time, SpriteBatch spriteBatch)
         {
             startSprite.Draw(time, spriteBatch, startDestinationRect);
-            textSprite.Draw(time, spriteBatch, textDestinationRect);
+            titleSprite.Draw(time, spriteBatch, titleDestinationRect);
+            menuSprite.Draw(time,spriteBatch,menuDestinationRect);
+        }
+
+        private string AddCursor(int index)
+        {
+            Contract.Assert(index >=0 && index < menuStrings.Length);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < menuStrings.Length; i++)
+            {
+                if (i == index)
+                {
+                    sb.Append(">");
+                }
+                sb.Append(menuStrings[i]);
+                if (i == index)
+                {
+                    sb.Append("<");
+                }
+                if (i != menuStrings.Length - 1)
+                {
+                    sb.Append("\n");
+                }
+            }
+            return sb.ToString();
         }
     }
 }
