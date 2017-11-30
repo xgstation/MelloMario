647e4e6a3d47d5283a6c32a51000fdf3e6642b0c
hcz <h.cz@qq.com>
2017-11-29 20:29:42 -0500

    Add files.

---
+++ MelloMario/MelloMario/LevelGen/Structures/Blocks.cs
@@ -0,0 +1,28 @@
+﻿namespace MelloMario.LevelGen.Terrains
+{
+    #region
+
+    using System;
+    using MelloMario.Objects.Blocks;
+    using MelloMario.Theming;
+    using Microsoft.Xna.Framework;
+
+    #endregion
+
+    internal class Blocks : IGenerator
+    {
+        private readonly IListener<IGameObject> listener;
+
+        public Blocks(IListener<IGameObject> listener)
+        {
+            this.listener = listener;
+        }
+
+        public void Request(IWorld world, Rectangle range)
+        {
+            world.Add(new Floor(world, new Point(range.Left, range.Bottom - (3 + Math.Abs(range.Left / 96 * 123456789 / 65535) % 5) * Const.GRID), listener)); // TODO: random
+            //world.Add(new Floor(world, new Point(range.Left, range.Bottom - 2 * Const.GRID), listener));
+            //world.Add(new Floor(world, new Point(range.Left, range.Bottom - 1 * Const.GRID), listener));
+        }
+    }
+}
