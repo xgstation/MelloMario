0263c04a2675efc77c70d95bafa810910b8fc6da
hcz <h.cz@qq.com>
2017-11-29 20:06:49 -0500

    Add forest terrain.

---
+++ MelloMario/MelloMario/LevelGen/Terrains/Forest.cs
@@ -0,0 +1,28 @@
+﻿namespace MelloMario.LevelGen.Terrains
+{
+    #region
+
+    using MelloMario.Objects.Blocks;
+    using MelloMario.Theming;
+    using Microsoft.Xna.Framework;
+
+    #endregion
+
+    internal class Forest : IGenerator
+    {
+        private readonly IListener<IGameObject> listener;
+
+        public Forest(IListener<IGameObject> listener)
+        {
+            this.listener = listener;
+        }
+
+        public void Request(IWorld world, Rectangle range)
+        {
+            for (int i = 1; i < 2 + range.Left * 23456789 / 32767 % 3; ++i)
+            {
+                world.Add(new Floor(world, new Point(range.Left, range.Bottom - i * Const.GRID), listener));
+            }
+        }
+    }
+}
