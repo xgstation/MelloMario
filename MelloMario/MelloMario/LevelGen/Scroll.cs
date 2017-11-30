3fa078840294ac2e738b560b7b931feef20277db
hcz <h.cz@qq.com>
2017-11-29 19:33:26 -0500

    Modify scroll generator.

--- MelloMario/MelloMario/LevelGen/Scroll.cs
+++ MelloMario/MelloMario/LevelGen/Scroll.cs
@@ -21,16 +21,17 @@
         {
             // note: top / buttom are locked
 
-            while (world.Boundary.Right < range.Right + Const.SCAN_RANGE)
-            {
-                terrains[0].Request(world, new Rectangle(world.Boundary.Right, world.Boundary.Top, Const.GRID, world.Boundary.Height)); // TODO
-                world.Extend(0, 32, 0, 0);
-            }
             while (world.Boundary.Left > range.Left - Const.SCAN_RANGE)
             {
-                terrains[0]
-                    .Request(world, new Rectangle(world.Boundary.Left - Const.GRID, world.Boundary.Top, Const.GRID, world.Boundary.Height)); // TODO
-                world.Extend(32, 0, 0, 0);
+                Rectangle subRange = new Rectangle(world.Boundary.Left - Const.GRID, world.Boundary.Top, Const.GRID, world.Boundary.Height);
+                terrains[range.Left / 1280 % terrains.Count].Request(world, subRange); // TODO
+                world.Extend(Const.GRID, 0, 0, 0);
+            }
+            while (world.Boundary.Right < range.Right + Const.SCAN_RANGE)
+            {
+                Rectangle subRange = new Rectangle(world.Boundary.Right, world.Boundary.Top, Const.GRID, world.Boundary.Height);
+                terrains[range.Left / 1280 % terrains.Count].Request(world, subRange); // TODO
+                world.Extend(0, Const.GRID, 0, 0);
             }
         }
     }
