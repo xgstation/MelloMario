9fa9adb4daf37bcc18ed2a6e896cfdbf08e8d773
hcz <h.cz@qq.com>
2017-11-30 23:44:23 -0500

    Add files.

---
+++ MelloMario/MelloMario/LevelGen/Generators/Structures/Bricks.cs
@@ -0,0 +1,27 @@
+﻿namespace MelloMario.LevelGen.Generators.Structures
+{
+    #region
+
+    using MelloMario.LevelGen.NoiseGenerators;
+    using MelloMario.Objects.Blocks;
+    using MelloMario.Theming;
+    using Microsoft.Xna.Framework;
+
+    #endregion
+
+    internal class Bricks : BaseGenerator
+    {
+        public Bricks(IListener<IGameObject> scoreListener, IListener<ISoundable> soundListener) : base(scoreListener, soundListener)
+        {
+        }
+
+        protected override void OnRequest(IWorld world, Rectangle range)
+        {
+            for (int j = range.Left; j < range.Right; j += Const.GRID)
+            {
+                int i = range.Bottom + 3 * Const.GRID;
+                AddObject("Brick", world, new Point(j, i));
+            }
+        }
+    }
+}
---
+++ MelloMario/MelloMario/LevelGen/Generators/Structures/Roof.cs
@@ -0,0 +1,27 @@
+﻿namespace MelloMario.LevelGen.Generators.Structures
+{
+    #region
+
+    using MelloMario.LevelGen.NoiseGenerators;
+    using MelloMario.Objects.Blocks;
+    using MelloMario.Theming;
+    using Microsoft.Xna.Framework;
+
+    #endregion
+
+    internal class Roof : BaseGenerator
+    {
+        public Roof(IListener<IGameObject> scoreListener, IListener<ISoundable> soundListener) : base(scoreListener, soundListener)
+        {
+        }
+
+        protected override void OnRequest(IWorld world, Rectangle range)
+        {
+            for (int j = range.Left; j < range.Right; j += Const.GRID)
+            {
+                int i = range.Top;
+                AddObject("Brick", world, new Point(j, i));
+            }
+        }
+    }
+}
