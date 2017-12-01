778bc2e2d8257d0d50c0d04e178c8f61f77f4345
hcz <h.cz@qq.com>
2017-11-30 19:57:34 -0500

    Tune random.

--- MelloMario/MelloMario/LevelGen/NoiseGenerators/PerlinNoiseGenerator.cs
+++ MelloMario/MelloMario/LevelGen/NoiseGenerators/PerlinNoiseGenerator.cs
@@ -95,26 +95,46 @@
             return BitConverter.ToInt32(bytes, 0) / (float) int.MaxValue * f;
         }
 
+        public static int Random(int seed, int x)
+        {
+            int x1 = x * 987654321 + seed * 123456789 + 55556666;
+            int q = x1 / 127773;
+            int r = x1 % 127773;
+            return Math.Abs(16807 * r - 2836 * q);
+        }
+
         public static Tuple<int, int> RandomSplit(int seed, int x, int scale)
         {
             // naive implementation
             // TODO: replace with smoothed pseudo-random
 
             int i = x;
-            while (Rand(seed, i) % scale != 0)
+            while (Random(seed, i) % scale != 0)
             {
                 i -= 1;
             }
 
             int j = x + 1;
-            while (Rand(seed, j) % scale != 0)
+            while (Random(seed, j) % scale != 0)
             {
                 j += 1;
             }
 
-            Debug.Print(i + " " + j);
-
             return new Tuple<int, int>(i, j);
+        }
+
+        public static int RandomProp(int seed, int x, int scale)
+        {
+            // naive implementation
+            // TODO: replace with smoothed pseudo-random
+
+            int i = x;
+            while (Random(seed, i) % scale != 0)
+            {
+                i -= 1;
+            }
+
+            return Random(seed, i + 1);
         }
 
         private void SwapInitial(int i, int j)
@@ -167,14 +187,6 @@
         private static float LerpPrecise(float a, float b, float k)
         {
             return MathHelper.LerpPrecise(a, b, k);
-        }
-
-        public static int Rand(int seed, int x)
-        {
-            int x1 = x * 987654321 + seed * 123456789 + 55556666;
-            int q = x1 / 127773;
-            int r = x1 % 127773;
-            return Math.Abs(16807 * r - 2836 * q);
         }
     }
 }
