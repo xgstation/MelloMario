12defcf30c6f60d771dee64bd059d9b5cce47afa
hcz <h.cz@qq.com>
2017-11-30 22:56:32 -0500

    Add generators.

---
+++ MelloMario/MelloMario/LevelGen/Generators/None.cs
@@ -0,0 +1,19 @@
+﻿namespace MelloMario.LevelGen.Generators.Objects
+{
+    #region
+
+    using System;
+    using MelloMario.LevelGen.NoiseGenerators;
+    using MelloMario.Objects.Blocks;
+    using MelloMario.Theming;
+    using Microsoft.Xna.Framework;
+
+    #endregion
+
+    internal class None : IGenerator
+    {
+        public void Request(IWorld world, Rectangle range)
+        {
+        }
+    }
+}
---
+++ MelloMario/MelloMario/LevelGen/Generators/Objects/Blocks.cs
@@ -0,0 +1,41 @@
+﻿namespace MelloMario.LevelGen.Generators.Objects
+{
+    #region
+
+    using System;
+    using MelloMario.LevelGen.NoiseGenerators;
+    using MelloMario.Objects.Blocks;
+    using MelloMario.Theming;
+    using Microsoft.Xna.Framework;
+
+    #endregion
+
+    internal class Blocks : BaseGenerator
+    {
+        public Blocks(IListener<IGameObject> scoreListener, IListener<ISoundable> soundListener) : base(scoreListener, soundListener)
+        {
+        }
+
+        protected override void OnRequest(IWorld world, Rectangle range)
+        {
+            int val = PerlinNoiseGenerator.Random(3902, (range.Left * 1100 + range.Top * 2367) / Const.GRID) % 40;
+
+            if (val < 1)
+            {
+                AddObject("Floor", world, range.Location);
+            }
+            else if (val < 2)
+            {
+                AddObject("Brick", world, range.Location);
+            }
+            else if (val < 3)
+            {
+                AddObject("Stair", world, range.Location);
+            }
+            else if (val < 4)
+            {
+                AddObject("Question", world, range.Location);
+            }
+        }
+    }
+}
